67851597780e4586a9754d5e1e65568f75d9fccd
Fakruddin Darvesh A K <darvesh696@yahoo.com>
2018-09-04 00:38:11 +0530

    uses config variables directly and removes unecessary variables

--- helpers/index.js
+++ helpers/index.js
@@ -1,25 +1,19 @@
-const FIRST_SEMESTER = CONFIG.FIRST_SEMESTER
-const THIRD_SEMESTER = CONFIG.THIRD_SEMESTER
-const FOUTH_SEMESTER = CONFIG.FOUTH_SEMESTER
-const FIFTH_SEMESTER = CONFIG.FIFTH_SEMESTER
-const SIXTH_SEMESTER = CONFIG.SIXTH_SEMESTER
-
 const getURL = function (semester) {
 	switch (semester) {
 		case '1':
-			return FIRST_SEMESTER
+			return CONFIG.FIRST_SEMESTER
 			break
 		case '2':
-			return THIRD_SEMESTER
+			return CONFIG.THIRD_SEMESTER
 			break
 		case '3':
-			return FOUTH_SEMESTER
+			return CONFIG.FOUTH_SEMESTER
 			break
 		case '4':
-			return FIFTH_SEMESTER
+			return CONFIG.FIFTH_SEMESTER
 			break
 		case '5':
-			return SIXTH_SEMESTER
+			return CONFIG.SIXTH_SEMESTER
 			break
 	}
 }
